8cbff7e2a0da0c54055ce999b452893d7126110a
Zaid2314 <mohammadzaid.2314@gmail.com>
2026-03-09 21:03:39 +0530

    Added QR code feature

--- controllers/url.js
+++ controllers/url.js
@@ -1,37 +1,56 @@
+const QRCode = require("qrcode");
 const shortid = require("shortid");
-const {URL} = require("../models/url");
+const { URL } = require("../models/url");
 const redisClient = require("../config/redis");
 
+async function handleGenerateNewShortURL(req, res) {
+    const body = req.body;
 
-async function handleGenerateNewShortURL(req,res) {
-    const body=req.body;
-    if(!body.url)
-        return res.status(400).json({error: "URL is required"});
+    if (!body.url)
+        return res.status(400).json({ error: "URL is required" });
 
     const shortID = shortid();
 
     await URL.create({
         shortId: shortID,
-        redirectUrl:body.url,
+        redirectUrl: body.url,
         visitHistory: []
     });
 
-     const allUrls = await URL.find({});
+    const allUrls = await URL.find({});
 
-     return res.render("home",{
-        id:shortID,
-         urls: allUrls,
-     })
+    // 🔥 Generate QR codes for each URL
+    const urlsWithQR = await Promise.all(
+        allUrls.map(async (url) => {
+
+            const shortUrl = `${req.protocol}://${req.get("host")}/url/${url.shortId}`;
+
+            const qrCode = await QRCode.toDataURL(shortUrl);
+
+            return {
+                ...url.toObject(),
+                qrCode
+            };
+        })
+    );
+
+    return res.render("home", {
+        id: shortID,
+        urls: urlsWithQR,
+    });
 }
 
-async function handleGetAnalytics(req,res) {
-        const shortId = req.params.shortId;
-        const result = await URL.findOne({shortId});
-        return res.json({
-            totalClicks: result.visitHistory.length,
-            analytics: result.visitHistory,
-        });
+async function handleGetAnalytics(req, res) {
+    const shortId = req.params.shortId;
+
+    const result = await URL.findOne({ shortId });
+
+    return res.json({
+        totalClicks: result.visitHistory.length,
+        analytics: result.visitHistory,
+    });
 }
+
 async function handleRedirect(req, res) {
 
     const shortId = req.params.shortId;
